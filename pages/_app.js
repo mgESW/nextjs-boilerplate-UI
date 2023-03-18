154dda8052c7df98f1451cf941b54aa64927a36c
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-18 16:59:48 -0700

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import SignIn from './signin';
+
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		
+	 <Route path='/sign-in' component={SignIn} />
+	</Routes>
+	</Router>
+);
 }
+
+export default App;
