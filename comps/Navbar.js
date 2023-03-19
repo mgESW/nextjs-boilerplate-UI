286c0a4c4a7bc88f2aa15e5bb5384e33471ac74c
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-19 00:17:58 -0700

    navbar3

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
+import styles from '@/styles/Home.module.css'
 
 const Navbar = () => {
     return (
       <nav>
         <div className="logo">
-          <h1></h1>
+          <h1>
+          </h1>
         </div>
 <Link legacyBehavior href={"/"}><a id="/">Home</a></Link>
-<Link legacyBehavior href={"/login"}><a id="/login">Login</a></Link>
+<Link legacyBehavior href={"https://vision-aid-prototype-v1.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fvision-aid-prototype-v1.vercel.app%2F"}><a id="https://vision-aid-prototype-v1.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fvision-aid-prototype-v1.vercel.app%2F">Login</a></Link>
       </nav>
   );
   }
--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,11 @@
+import Navbar from '../comps/Navbar'
+
 const Login = () => {
     return (
       <div>
+<Navbar />
         <h1>Login</h1>
+        
         <p>Admin Login</p>
 
       </div>
