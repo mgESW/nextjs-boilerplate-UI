230cfb7c4285d1d6df4333728b886523e49fb9c9
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-18 22:02:39 -0700

    Update Navbar.js

---
+++ comps/Navbar.js
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+    return (
+      <nav>
+        <div className="logo">
+          <h1>
+          </h1>
+        </div>
+       <a>Home</a>
+       <a>Login</a>
+      </nav>
+  );
+  }
+   
+  export default Navbar;
