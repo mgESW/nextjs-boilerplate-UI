09b98b1d9d8a0a0f8e61bddfdf4489c007b1c593
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-19 19:45:05 -0700

    NavBar Seth

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,20 +1,37 @@
 import Link from "next/link";
 import styles from '@/styles/Home.module.css'
+import {useSession, signIn, signOut, getSession} from "next-auth/react";
 
 
-const Navbar = () => {
+export default function Navbar() {
+
+    const {data: session} = useSession();
     return (
+    <div>
       <nav>
         
         <div className="logo">
           <h1>
           </h1>
-        </div>
+          
+{!session ? (
+                <div>
+                    <p>Not signed in</p>
+                    <br/>
+                    <button onClick={() => signIn()}>Sign in</button>
+                </div>
+            ) : (
+                <div>
+                    <h4>Signed in as {session.user.email}</h4>
+                    <button onClick={() => signOut()}>Sign out</button>
+                </div>
+            )}
 <Link legacyBehavior href={"/"}><a id="/">Home</a></Link>
-<Link legacyBehavior href={"https://vision-aid-prototype-v1.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fvision-aid-prototype-v1.vercel.app%2F"}><a id="https://vision-aid-prototype-v1.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fvision-aid-prototype-v1.vercel.app%2F">Login</a></Link>
-
+            </div>
+            
       </nav>
+      </div>
   );
   }
    
-  export default Navbar;
+ 
