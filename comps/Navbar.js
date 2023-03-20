4281955a431ee2a04e7defc7fda32fb56a14ae3b
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-19 17:01:12 -0700

    Update Navbar.js

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import styles from '@/styles/Home.module.css'
-
+/*import {useSession, signIn, signOut, getSession} from "next-auth/react";*/
 const Navbar = () => {
     return (
       <nav>
+        
         <div className="logo">
           <h1>
           </h1>
         </div>
 <Link legacyBehavior href={"/"}><a id="/">Home</a></Link>
 <Link legacyBehavior href={"https://vision-aid-prototype-v1.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fvision-aid-prototype-v1.vercel.app%2F"}><a id="https://vision-aid-prototype-v1.vercel.app/api/auth/signin?callbackUrl=https%3A%2F%2Fvision-aid-prototype-v1.vercel.app%2F">Login</a></Link>
+
       </nav>
   );
   }
