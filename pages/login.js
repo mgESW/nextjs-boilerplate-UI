252763371496da8a988a48d4e09f74eece509478
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-24 16:02:10 -0700

    Revert "com1"
    This reverts commit ac6763d311b055edc42246c9e559ab1346d1eac0.

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import Navbar from '../comps/Navbar'
+
+const Login = () => {
+    return (
+      <div>
+<Navbar />
+        <h1>Login</h1>
+        
+        <p>Admin Login</p>
+
+      </div>
+    );
+  }
+   
+  export default Login;
