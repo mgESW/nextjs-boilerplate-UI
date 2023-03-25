068433d6f629ca7d770b89b9460becaeb6ad29ce
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-25 11:13:43 -0700

    Update tech.js

--- pages/tech.js
+++ pages/tech.js
@@ -73,10 +73,11 @@ height={144}
 width={144}
 alt=""
 />
-<div className="mx-auto max-w-2xl text-center">
-<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Vision Aid Partner Tracking
+<div className={styles.center}>
+<h2 className={styles.main}>
+Vision Aid Partner Tracking
     System</h2>
-<p className="mt-2 text-lg leading-8 text-gray-600">
+<p className={styles.grid}>
     This form is for submitting the personal information of a beneficiary
 </p>
 </div>
