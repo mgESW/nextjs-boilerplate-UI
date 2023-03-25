1072838e9f49fe2d36ca69f565482474e9eda015
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-24 17:18:41 -0700

    Revert "Update tech.js"
    This reverts commit 47907aaf79336f51b55d4a0016cec31e76a03624.

--- pages/tech.js
+++ pages/tech.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+export default function Tech() {
     return (
     <> 
 <Image
